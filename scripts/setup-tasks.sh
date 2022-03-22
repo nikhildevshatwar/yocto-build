d810a31a6be766c5c7d362a83dcda72c6d4b98f9
Nikhil Devshatwar <nikhil.nd@ti.com>
2022-03-23 03:42:06 +0530

    test
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- scripts/setup-tasks.sh
+++ scripts/setup-tasks.sh
@@ -57,7 +57,7 @@ install_host_packages() {
 		git build-essential diffstat texinfo gawk chrpath socat doxygen \
 		dos2unix python python3 bison flex libssl-dev u-boot-tools mono-devel \
 		mono-complete curl python3-distutils repo pseudo python3-sphinx \
-		g++-multilib libc6-dev-i386 jq
+		g++-multilib libc6-dev-i386 jq cpio
 
 	# Install packages required for debugging
 	sudo -E apt-get -f -y install \
